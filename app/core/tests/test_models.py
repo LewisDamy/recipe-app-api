d575ef09f30b8b3837a1bcd3b4b449ecdd7397c8
Lewis_Damy <ldamy16@gmail.com>
2024-01-18 00:32:29 -0300

    feat: user API done

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,7 +17,7 @@ class ModelTest(TestCase):
             password=password
         )
 
-        self.assertEquals(user.email, email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
@@ -26,26 +26,21 @@ class ModelTest(TestCase):
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
             ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
-            ['test4@example.com', 'test4@example.com']]
+            ['test4@example.COM', 'test4@example.com']]
 
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
-            self.assertEquals(user.email, expected)
+            self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(
-                email='',
-                password='sample123'
-            )
+            get_user_model().objects.create_user('', 'sample123')
 
     def test_create_superuser(self):
         user = get_user_model().objects.create_superuser(
             email='test@example.com',
             password='sample123'
         )
-        self.assertTrue(user)
-        self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
-        
+        self.assertTrue(user.is_staff)
--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,48 +1,42 @@
-"""
-Serializers for the user API View.
-"""
 from django.contrib.auth import (
     get_user_model,
     authenticate
 )
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
+
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer for the user object."""
-    """
-    This class is where we tell the Django Rest Framework, the model and the fields and any additional arguments
-    that we want to pass to the serialize set and the serialized needs to know which model it's representing.
-    """
+    """Serializer for the users object"""
 
     class Meta:
-        model = get_user_model()  # this serializer is going to be for our user model
-        fields = ['email', 'password',
-                  'name']  # list of fields that we want to make available through the serialization
-        # This fields must be only the ones that the user will be able to change through the API
-
-        # Dictionary that allows us to provide extra metadata to provide different fields, by telling DRF things like we
-        # do want the fields to be write only or read only, ...
+        model = get_user_model()
+        fields = ('email', 'password', 'name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
-        """
-        The Create method allows us to override the behavior that the serializer does when you create a new user object
-        out of that serialize. By doing this the create method will be called after validation and it will only be called 
-        after the validation was successful. If the validation error, i.e., the minimum wasn't provided the min length,
-        then would just raise a validation error.
-        """
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
 
-        def create(self, validated_data):
-            """Create and return a user encrypted password."""
-            return get_user_model().objects.create_user(**validated_data)
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        password = validated_data.pop('password', None)
+        # Updating the existing model serializer and updating only the necessary
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
-    """Serializer for the user auth token."""
-    email = serializers.EmailField()
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
     password = serializers.CharField(
-        style={'input_type', 'password'},
+        style={'input_type': 'password'},
         trim_whitespace=False
     )
 
@@ -50,13 +44,14 @@ class AuthTokenSerializer(serializers.Serializer):
         """Validate and authenticate the user"""
         email = attrs.get('email')
         password = attrs.get('password')
+
         user = authenticate(
             request=self.context.get('request'),
             username=email,
             password=password
         )
         if not user:
-            msg = _('Unabled to authenticate with provided credentials.')
+            msg = _('Unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authorization')
 
         attrs['user'] = user
